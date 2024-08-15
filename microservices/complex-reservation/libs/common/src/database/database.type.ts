19ee8857e71e03d65418a38d59a3f1360858e808
Kasir Barati <kasir.barati@gmail.com>
2024-08-15 10:03:50 +0200

    chore: add abstract repo + schema

--- microservices/complex-reservation/libs/common/src/database/database.type.ts
+++ microservices/complex-reservation/libs/common/src/database/database.type.ts
@@ -2,3 +2,12 @@ export interface DatabaseConfig {
   DATABASE_URL: string;
   MONGO_INITDB_DATABASE: string;
 }
+export interface Pagination<Document> {
+  page: number;
+  limit: number;
+  total: number;
+  lastPage: number;
+  prev: number | null;
+  next: number | null;
+  data: Document[];
+}
