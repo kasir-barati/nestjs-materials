9259dffb0adb10d62f56515627ce460a63f3ded0
Kasir Barati <kasir.barati@gmail.com>
2024-08-20 15:53:01 +0200

    chore: wrote jsdoc for clarity

--- microservices/complex-reservation/libs/common/src/utils/login.util.ts
+++ microservices/complex-reservation/libs/common/src/utils/login.util.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+/**
+ * @returns {string} authentication JWT cookie
+ */
 export async function login(email: string, password: string) {
   const { headers } = await axios.post(
     `http://localhost:${process.env.AUTH_SERVICE_PORT}/auth/login`,
