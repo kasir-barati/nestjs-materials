f23922838ea5feb085493228d9daec2422bb413f
Mohammad Jawad Barati <kasir.barati@gmail.com>
2025-06-04 15:06:27 +0200

    chore: add util to index service

--- mongoose/documentdb-index-creation/src/app/services/index.service.ts
+++ mongoose/documentdb-index-creation/src/app/services/index.service.ts
@@ -1,6 +1,6 @@
 import { Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
-import { Connection } from 'mongoose';
+import { Connection, Model } from 'mongoose';
 
 export class IndexService implements OnApplicationBootstrap {
   private readonly logger = new Logger(IndexService.name);
@@ -34,6 +34,49 @@ export class IndexService implements OnApplicationBootstrap {
 
         throw err;
       }
+    }
+  }
+
+  private async createIndexIfTheyDoNotExists(
+    modelName: string,
+    model: Model<unknown, unknown, unknown, object, unknown, unknown>,
+  ) {
+    const existingIndexes = await model.collection.indexes();
+    const existingIndexesNames = existingIndexes
+      .map((index) => index.name)
+      .filter(Boolean) as string[];
+    const definedIndexes = model.schema.indexes();
+    const missingIndexes = definedIndexes.filter(
+      ([_, options]) =>
+        options?.name && !existingIndexesNames.includes(options.name),
+    );
+
+    if (missingIndexes.length > 0) {
+      this.logger.log(
+        `⏳ Creating ${missingIndexes.length} indexes for ${modelName}: ${missingIndexes.map(([_, options]) => options.name).join(', ')}`,
+      );
+
+      for (const [spec, options] of missingIndexes) {
+        // @ts-expect-error This is OK
+        await model.collection.createIndex(spec, options);
+      }
+
+      this.logger.log(`✅ Created indexes for ${modelName}`);
+    } else {
+      this.logger.log(`✅ All indexes exist for ${modelName}`);
+    }
+
+    const definedIndexesNames = definedIndexes
+      .map(([_, options]) => options.name)
+      .filter(Boolean) as string[];
+    const extraIndexes = existingIndexesNames.filter(
+      (name) => !definedIndexesNames.includes(name),
+    );
+
+    if (extraIndexes.length > 0) {
+      this.logger.log(
+        `ℹ️ Extra indexes in DB for ${modelName}: ${extraIndexes.join(', ')}`,
+      );
     }
   }
 }
