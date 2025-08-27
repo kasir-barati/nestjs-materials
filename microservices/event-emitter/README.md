814394053e164736c4a70b450e315ba5aace0c6b
Mohammad Jawad Barati <kasir.barati@gmail.com>
2025-08-27 13:33:03 +0200

    fix the issue of returned value from event emitters

--- microservices/event-emitter/README.md
+++ microservices/event-emitter/README.md
@@ -2,3 +2,4 @@
 
 - It does make a difference when we have multiple listeners.
 - Especially if you are returning a value.
+- But you can probe all returned values and return only the one that ain't empty as we did [here](./src/services/app.service.ts).
--- microservices/event-emitter/src/services/app.service.ts
+++ microservices/event-emitter/src/services/app.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
-import { isEmpty } from 'class-validator';
+import { isEmpty, isNotEmpty } from 'class-validator';
 
 @Injectable()
 export class AppService {
   constructor(private eventEmitter: EventEmitter2) {}
 
   async getHelloKasir(): Promise<string> {
-    const response = await this.eventEmitter.emitAsync('some-event', 'Kasir');
-    const result = response[0] as string;
+    const responses = await this.eventEmitter.emitAsync('some-event', 'Kasir');
+    const result = responses.find((response) => isNotEmpty(response)) as string;
 
     if (isEmpty(result)) {
       throw new Error('Missing message');
